4c62e4e196bde92e45d032b330ae9c589cc42083
saranonuan <saranonuan@gmail.com>
2018-02-20 08:31:57 +0700

    Remove distinctUntilChanged

--- Source/Observable+Optional.swift
+++ Source/Observable+Optional.swift
@@ -87,19 +87,3 @@ public extension ObservableType where E: OptionalType {
         }
     }
 }
-
-public extension ObservableType where E: OptionalType, E.Wrapped: Equatable {
-    /**
-     Returns an observable sequence that contains only distinct contiguous elements according to equality operator.
-     
-     - seealso: [distinct operator on reactivex.io](http://reactivex.io/documentation/operators/distinct.html)
-     
-     - returns: An observable sequence only containing the distinct contiguous elements, based on equality operator, from the source sequence.
-     */
-    
-    public func distinctUntilChanged() -> Observable<Self.E> {
-        return self.distinctUntilChanged { (lhs, rhs) -> Bool in
-            return lhs.value == rhs.value
-        }
-    }
-}
